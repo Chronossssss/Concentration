4e01d8e3f79db6d3881da7843a9f1fc97b21a2b8
xiaolin qian <michael@xiaolins-imac.local>
2018-07-31 14:29:09 -0400

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by xiaolin qian on 7/31/18.
+//  Copyright © 2018 xiaolin qian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
