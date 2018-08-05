d5075a1382d5e00427c6e6c8850ad7509b116610
michael950513@gmail.com <qxj680501>
2018-08-05 18:41:48 -0400

    init

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -6,15 +6,46 @@
 //  Copyright © 2018 xiaolin qian. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var count = 0 {
+        didSet {
+            clickCountLabel.text = "Click Time: \(count)"
+        }
+    }
+    
+    let emojis = ["🍎","🍊","🍇","🍈"]
+ 
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    
+    @IBOutlet weak var clickCountLabel: UILabel!
+    
+    @IBAction func TouchCard1(_ sender: UIButton) {
+        count += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            flicCard(callEmoji: emojis[cardNumber], on: sender)
+        
+        
+        }
+    }
+    
+    func flicCard(callEmoji insideEmoji:String, on button: UIButton) {
+        if (button.currentTitle == insideEmoji) {
+            button.setTitle("", for: UIButton.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9973798394, green: 0.5146915317, blue: 0.9789187312, alpha: 1)
+        }
+        else {
+            button.setTitle(insideEmoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
     }
 
-
 }
+
+
 
